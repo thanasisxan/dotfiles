cebeb43b00caaf807433066a6c40d45fc7a3399b
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2026-01-21 23:02:53 +0200

    lazyvim stuff

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+vim.opt.relativenumber = true
+
+vim.diagnostic.enable(false)
+
+vim.g.autoformat = false
